e445a85efdb2e576cdb44adfee9d29a862fc7782
github username <github email address>
2019-04-02 18:06:37 -0400

    uploading this to github so we can work in tandem

--- src/identicon.js
+++ src/identicon.js
@@ -1,3 +1,25 @@
+const blockColor = "rgb(16, 244, 68)"
 class Identicon {
   // make use of the md5.array method, which is in scope because we required it in our index.html
+  constructor(input) {
+    this.identicon = input
+  }
+
+  hashIdenticon() {
+    const hashed = md5.array(this.identicon)
+    return hashed
+  }
+
+  cssRenderer() {this.hashIdenticon().map(x => x%2 === 0 ? 1 : 0).map(function(num, index)
+    {if (num ===1)
+      {document.getElementById(`${Math.floor(index/5)}-${index%5}`).style.backgroundColor = blockColor} })
+
+  }
+
+  // const rowIdx = 0
+  // const columnIdx = 4
+
+  // document.getElementById(`${rowIdx}-${colIdx}`).style.backgroundColor = blockColor
+
+
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 function handleSubmit(e) {
+  e.preventDefault()
+  const userInput = document.getElementById('user-input').value
 
+  const ident = new Identicon(userInput)
+  console.log(ident)
+  console.log(ident.hashIdenticon())
+  ident.cssRenderer()
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
 })
+
+
+// const rowIdx = 0
+// const columnIdx = 4
+// const blockColor = "rgb(16, 244, 68)"
+// document.getElementById(`${rowIdx}-${colIdx}`).style.backgroundColor = blockColor
